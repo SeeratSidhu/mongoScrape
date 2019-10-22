316aa5a8c1b1de845625def3bab2adf729f41cf6
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-10-22 19:12:28 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+app.get("/scrape", function(req, res){
+    axios.get("https://www.nytimes.com/").then(function(response){
+        var $ =  cheerio.load(response.data);
+    });
+});
