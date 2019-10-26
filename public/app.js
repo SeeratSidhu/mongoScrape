ea5971ece22458e1c106256019d6f4e6e93b6679
Manseerat Kaur <seeratsidhu01@gmail.com>
2019-10-26 19:45:34 -0400

    code clean up

--- public/app.js
+++ public/app.js
@@ -6,7 +6,8 @@ function displayNews(data) {
                 <h5 class="card-header"><a href = "${data[i].link}" target = "_blank">${data[i].title}</a></h5>
                 <div class="card-body">
                     <p class="card-text">${data[i].body}</p>
-                    <a href="#" class="btn btn-primary" class="comment">Leave a Comment</a>
+                    <button type="button" class="btn btn-info btn-lg" id= "commentButton" data-id=${data[i].id}>Leave Comment</button>
+                    
                 </div>
             </div>`;
         $("#newsCard").append(card);
@@ -23,4 +24,7 @@ $("#scrapeButton").on("click", function () {
     });
 });
 
-$
+$("#newsCard").on("click", "#commentButton", function(){
+    console.log("modal ")
+    
+});
